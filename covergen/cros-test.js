c509e482cbd47bbe4871527e10580d99eac75087
sky48 <bradley@imaginationroom.co>
2018-07-27 13:44:00 -0700

    more testing on chrome blocking dl

--- covergen/cros-test.js
+++ covergen/cros-test.js
@@ -2,13 +2,20 @@ function downloadAsPic() {
     html2canvas(document.querySelector("#canvas_container")).then(canvas => {
         document.body.appendChild(canvas);
         console.log(canvas);
-        let url = canvas.toDataURL();
+        let url = '';
+        try {
+            url = canvas.toDataURL();
+        }
+        catch (err) {
+            console.log(err);
+        }
         let link = document.createElement('a');
         link.href = url;
         link.download = "cover.png";
+        link.text = "press me to download"
         document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link);
+        // document.body.removeChild(link);
         document.body.removeChild(canvas);
     });
 }
