efe1ec760dc4a3dc51746f181556525356bc5b9e
sky48 <bradley@imaginationroom.co>
2018-07-27 14:07:40 -0700

    test with jpg

--- covergen/cros-test.js
+++ covergen/cros-test.js
@@ -4,14 +4,14 @@ function downloadAsPic() {
         console.log(canvas);
         let url = '';
         try {
-            url = canvas.toDataURL();
+            url = canvas.toDataURL('image/jpeg', 0.5);
         }
         catch (err) {
             console.log(err);
         }
         let link = document.createElement('a');
         link.href = url;
-        link.download = "cover.png";
+        link.download = "cover.jpg";
         link.text = "press me to download"
         document.body.appendChild(link);
         link.click();
