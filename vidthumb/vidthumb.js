41e1417b604cb53a5a9467ddb4287c82c2f93eee
sky48 <4812875@gmail.com>
2018-08-03 15:39:02 -0700

    Wrapped video processing flow with Promises.

--- vidthumb/vidthumb.js
+++ vidthumb/vidthumb.js
@@ -1,45 +1,52 @@
 let video_canvas = document.getElementById('video-canvas');
-let video_player = document.getElementById('video-player');
 
-function loadVideo() {
-    // load the file to video-player:
-    let src = URL.createObjectURL(document.querySelector("#file-to-upload").files[0]);
-    video_player.setAttribute('src', src);
-    video_player.load();
+function go() {
+    console.log('going');
 
-    // Load metadata of the video to get video duration and dimensions
-    video_player.addEventListener('loadedmetadata', function () {
-        console.log("Total length: %ds.", video_player.duration);
-        for (i = 0; i < video_player.duration; i += 5) { // TODO - can't rapid fire like this 
-            seekTo(i);
-        }
-        video_canvas.width = video_player.videoWidth  / 2;
-        video_canvas.height = video_player.videoHeight / 2;
+    initVideo().then((vplayer) => {
+        console.log('Total video length: %ds', vplayer.duration);
+        return seekTo(vplayer, 3); // seek to 3 seconds
+    }).then((vplayer) => {
+        console.log('Video is now paused at the requested time.');
+        //generate thumbnail URL data
+        video_canvas.width = vplayer.videoWidth / 2;
+        video_canvas.height = vplayer.videoHeight / 2;
+
+        let context = video_canvas.getContext('2d');
+        context.drawImage(vplayer, 0, 0, vplayer.videoWidth / 2, vplayer.videoHeight / 2);
+        let data = video_canvas.toDataURL();
+        //create img
+        let img = document.createElement('img');
+        img.setAttribute('src', data);
+        //append img in container div
+        document.getElementById('preview-container').appendChild(img);
+    });
+
+    console.log('gone');
+}
+
+function initVideo() {
+    return new Promise((resolve, reject) => {
+        let video_player = document.createElement('video');
+        document.getElementById('hidden-container').appendChild(video_player); // debug use
+        // load the file to video-player:
+        let src = URL.createObjectURL(document.querySelector('#file-to-upload').files[0]);
+        video_player.setAttribute('src', src);
+        video_player.load();
+
+        // Load metadata of the video to get video duration and dimensions
+        video_player.addEventListener('loadedmetadata', function () {
+            resolve(video_player);
+        });
     });
 }
 
-function go() {
-    console.log('gone')
-}
-
-function seekTo(second) {
-    video_player.currentTime = second;
-}
-
-video_player.addEventListener("seeked", onSeekComplete);
-
-function onSeekComplete(e) {
-    console.log("seeked event fired");
-
-    //generate thumbnail URL data
-    let context = video_canvas.getContext("2d");
-    context.drawImage(video_player, 0, 0, video_player.videoWidth / 2, video_player.videoHeight / 2);
-    let data = video_canvas.toDataURL();
-
-    //create img
-    var img = document.createElement('img');
-    img.setAttribute('src', data);
-
-    //append img in container div
-    document.getElementById('preview-container').appendChild(img);
+function seekTo(vplayer, second) {
+    return new Promise((resolve, reject) => {
+        console.log('Promised to seek to %ds.' + second);
+        vplayer.currentTime = second;
+        vplayer.addEventListener('seeked', () => {
+            resolve(vplayer);
+        });
+    });
 }
