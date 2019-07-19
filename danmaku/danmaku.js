69ecbaa5f964c10df59883c97cd38b06e37eb7fb
sky-48 <4812875@gmail.com>
2019-07-18 23:48:04 -0700

    Add dispatcher to group danmaku into lines

--- danmaku/danmaku.js
+++ danmaku/danmaku.js
@@ -1,19 +1,29 @@
 class Danmaku {
-    constructor(text, delay, line, color) {
-        this.text = text || "<missing_text>";
-        this.color = color || "white";
-        this.line = line || 1;
+  constructor(text) {
+    this.text = text || "<missing_text>";
+  }
 
-        this.left = w * delay / 2;
+  run(line) {
+    this.line = line;
+    this.left = w;
+    this.color = Danmaku.getColor();
+    this.velocity = Math.max(this.text.length / 2, 2);
+    window.requestAnimationFrame(() => this.draw());
+  }
 
-        // calculate velocity:
-        this.velocity = Math.max(text.length / 2, 2);
+  draw() {
+    this.left -= this.velocity;
+    ctx.fillStyle = this.color;
+    ctx.fillText(this.text, this.left, this.line * 30);
+    if (this.left > -1000) {
+      window.requestAnimationFrame(() => this.draw());
     }
+  }
 
-    draw(ctx) {
-        if (this.left < -1000) return;
-        this.left -= this.velocity;
-        ctx.fillStyle = this.color;
-        ctx.fillText(this.text, this.left, this.line * 30);
-    }
+  static getColor() {
+    const rnd = Math.random() * 100;
+    if (rnd < 10) return "yellow";
+    if (rnd < 20) return "red";
+    return "white";
+  }
 }
--- danmaku/runner.js
+++ danmaku/runner.js
@@ -1,35 +1,63 @@
-const w = 600, h = 400;
+const w = 600;
+const h = 400;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
-ctx.clear = function () {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, w, h);
+ctx.clear = function() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, w, h);
 };
 ctx.clear();
-
+var dispatchers;
 const danmakus = [];
 function go() {
-    document.getElementById("btnGo").disabled = true;
+  let texts = document
+    .getElementById("input-text")
+    .value.trim()
+    .split("\n");
+  texts.push(...texts);
+  texts.push(...texts);
+  texts.push(...texts);
 
-    for (let i = 0; i < 100; i++) {
-        danmakus.push(new Danmaku("test", i, getLine(), "red"));
-        danmakus.push(new Danmaku("2333333", i, getLine()));
-        danmakus.push(new Danmaku("666666", i, getLine()));
-        danmakus.push(new Danmaku("awsl", i, getLine(), "yellow"));
-    }
+  dispatchers = [...Array(11).keys()].map(id => new Dispatcher(id));
 
-    window.requestAnimationFrame(tick);
+  document.getElementById("btnGo").disabled = true;
+
+  texts.map(text => new Danmaku(text)).forEach(d => danmakus.push(d));
+
+  window.requestAnimationFrame(clear);
+
+  danmakus.forEach(d => {
+    dispatchers
+      .sort((a, b) => a.getCooldown() - b.getCooldown())[0]
+      .dispatch(d);
+  });
 }
 
-let line = 0;
-function getLine() {
-    line++;
-    return (line % 10) + 1;
+function clear(timestamp) {
+  ctx.clear();
+  window.requestAnimationFrame(clear);
 }
 
-function tick(timestamp) {
-    ctx.clear();
-    danmakus.forEach(d => d.draw(ctx));
-    window.requestAnimationFrame(tick);
+class Dispatcher {
+  constructor(id) {
+    this.id = id;
+    this.cd = 0;
+  }
+
+  getCooldown() {
+    return this.cd + this.id;
+  }
+
+  dispatch(danmaku) {
+    console.log(this.id + " dispatching " + danmaku.text);
+    danmaku.run(this.id + 1);
+    this.cd += 2000;
+  }
+
+  // todo - have a timer call this
+  cooldown(value) {
+    this.cd -= value;
+    if (this.cd < 0) this.cd = 0;
+  }
 }
